90bff77dbfabb7081e89ccba0d8c9974219163c8
happy birhtday <atienovallary9@gmail.com>
2022-05-31 15:29:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton:Button=findViewById(R.id.button)
+        rollButton.setOnClickListener{
+           val toast=Toast.makeText(this,"Dice Rolled!!", Toast.LENGTH_LONG)
+           toast.show()
+            val resultTextView:TextView=findViewById(R.id.textView)
+            resultTextView.text="6"
+
+        }
+    }
+}
